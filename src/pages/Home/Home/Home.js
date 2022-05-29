6be9bcffe51eb133951c480a71136407f7035b89
kamrulhaider <kamrulhaider.ssb@gmail.com>
2022-05-29 09:38:39 +0600

    [add]skill section

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Contact from '../Contact/Contact';
-import Projects from '../Projects/Projects';
-import shape1 from '../../../images/shape2.png';
-import shape2 from '../../../images/logo192.png';
+import React from "react";
+import shape2 from "../../../images/logo192.png";
+import shape1 from "../../../images/shape2.png";
+import Banner from "../Banner/Banner";
+import Contact from "../Contact/Contact";
+import Projects from "../Projects/Projects";
+import Technology from "../Technology/Technology";
 
 const Home = () => {
-    return (
-        <div className="container">
-            <img className='shape4' src={shape1} alt="" />
-            <img className='shape5' src={shape2} alt="" />
-            <Banner></Banner>
-            <Projects></Projects>
-            <Contact></Contact>
-        </div>
-    );
+  return (
+    <div className="container">
+      <img className="shape4" src={shape1} alt="" />
+      <img className="shape5" src={shape2} alt="" />
+      <Banner></Banner>
+      <Technology></Technology>
+      <Projects></Projects>
+      <Contact></Contact>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/Technology/Technology.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Technology() {
+  return (
+    <div className="container my-3">
+      <div className="d-flex flex-column align-items-center mb-3">
+        <h2 className="fw-bold text-center title-color mb-0">
+          My Skills & Familiarity
+        </h2>
+        <hr />
+        <p className="text-center subTitle-color mb-3">
+          Here are the top projects that I have done throughout my Journey of
+          web development.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Technology;
